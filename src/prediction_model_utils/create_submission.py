692b6752df3b78bd095574670771472b9b31bf27
jstafford <jwstafford1991@gmail.com>
2025-10-21 21:43:20 -0500

    Fixed runtime errors

--- src/prediction_model_utils/create_submission.py
+++ src/prediction_model_utils/create_submission.py
@@ -2,10 +2,13 @@ from typing import Any
 
 from pandas import DataFrame
 
+from src.constants import PASSENGER_ID_HEADER
+
+
 def create_submission_csv(titanic_df: DataFrame, predictions: Any) -> DataFrame:
 	random_forest_submission_df = DataFrame({
-		'PassengerId': titanic_df['PassengerId'],
-		'Survived': titanic_df
+		'PassengerId': titanic_df[PASSENGER_ID_HEADER],
+		'Survived': predictions
 	})
 
 	random_forest_submission_df.to_csv('../data/random_forest_submission.csv', index=False)
--- src/prediction_model_utils/data_massager.py
+++ src/prediction_model_utils/data_massager.py
@@ -1,11 +1,30 @@
 from pandas import DataFrame
 
-from src.constants import SURVIVED_HEADER, SEX_HEADER, AGE_HEADER, PCLASS_HEADER, SEX_ENCODED
+from src.constants import SURVIVED_HEADER, SEX_HEADER, AGE_HEADER, PCLASS_HEADER, SEX_ENCODED, PASSENGER_ID_HEADER
 
 
-def message_data(titanic_df: DataFrame) -> DataFrame:
+def message_training_data(titanic_df: DataFrame) -> DataFrame:
 	titanic_features_df: DataFrame = titanic_df[
 		[SURVIVED_HEADER, SEX_HEADER, AGE_HEADER, PCLASS_HEADER]].copy()
-	titanic_features_df[SEX_ENCODED] = titanic_features_df[SEX_HEADER].map({'male': 0, 'female': 1})
-	titanic_features_df[AGE_HEADER] = titanic_features_df[AGE_HEADER].fillna(titanic_features_df[AGE_HEADER].median())
-	return titanic_features_df
+	return message_data(titanic_features_df)
+
+def message_test_data(titanic_df: DataFrame) -> DataFrame:
+	titanic_features_df: DataFrame = titanic_df[
+		[PASSENGER_ID_HEADER, SEX_HEADER, AGE_HEADER, PCLASS_HEADER]].copy()
+	return message_data(titanic_features_df)
+
+def message_data(titanic_features_df: DataFrame) -> DataFrame:
+	titanic_features_messaged_df = titanic_features_df.copy()
+	titanic_features_messaged_df = encode_sex(titanic_features_messaged_df)
+	titanic_features_messaged_df = fill_missing_age(titanic_features_messaged_df)
+	return titanic_features_messaged_df
+
+def encode_sex(titanic_features_df: DataFrame) -> DataFrame:
+	titanic_features_with_encoded_sex_df = titanic_features_df.copy()
+	titanic_features_with_encoded_sex_df[SEX_ENCODED] = titanic_features_df[SEX_HEADER].map({'male': 0, 'female': 1})
+	return titanic_features_with_encoded_sex_df
+
+def fill_missing_age(titanic_features_df: DataFrame) -> DataFrame:
+	titanic_features_filled_ages_df = titanic_features_df.copy()
+	titanic_features_filled_ages_df[AGE_HEADER] = titanic_features_df[AGE_HEADER].fillna(titanic_features_df[AGE_HEADER].median())
+	return titanic_features_filled_ages_df
